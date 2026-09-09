61e9f704d4630c523abcd1bbafdd2624ec091a7a
bhumikam925 <bhumikam835@gmail.com>
2026-09-10 02:46:13 +0530

    Implement CustomModal component with shadow DOM

--- components/CustomModal.js
+++ components/CustomModal.js
@@ -1,1 +1,22 @@
+import { } from "../core/store.js";
 
+class CustomModal extends HTMLElement {
+
+    constructor() {
+        super();
+
+        this.attachShadow({ mode: "open" });
+
+        const template =
+            document.getElementById("modal-template");
+
+        this.shadowRoot.appendChild(
+            template.content.cloneNode(true)
+        );
+    }
+}
+
+customElements.define(
+    "custom-modal",
+    CustomModal
+);
