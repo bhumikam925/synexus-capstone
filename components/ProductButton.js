ccab570cc9d6e845b7ac7642b08bc12eacaa0717
bhumikam925 <bhumikam835@gmail.com>
2026-09-10 02:47:12 +0530

    Implement ProductButton component with cart functionality

--- components/ProductButton.js
+++ components/ProductButton.js
@@ -1,1 +1,36 @@
+import { globalStore } from "../core/store.js";
 
+class ProductButton extends HTMLElement {
+    constructor() {
+        super();
+
+        this.attachShadow({ mode: "open" });
+
+        this.shadowRoot.innerHTML = `
+            <style>
+                button {
+                    padding: 10px 18px;
+                    border: none;
+                    border-radius: 8px;
+                    cursor: pointer;
+                    font-size: 16px;
+                    font-weight: bold;
+                }
+            </style>
+
+            <button id="add-to-cart">Add to Cart</button>
+        `;
+    }
+
+    connectedCallback() {
+        const button = this.shadowRoot.querySelector("#add-to-cart");
+
+        button.addEventListener("click", () => {
+            globalStore.setState({
+                cartCount: globalStore.state.cartCount + 1
+            });
+        });
+    }
+}
+
+customElements.define("product-button", ProductButton);
