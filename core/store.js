d7579a8d9dad7bd182a5160d5d03a2dc754bec64
bhumikam925 <bhumikam835@gmail.com>
2026-09-10 02:48:47 +0530

    Implement StateStore class for state management

--- core/store.js
+++ core/store.js
@@ -1,1 +1,28 @@
 
+class StateStore {
+    constructor(initialState) {
+        this.state = initialState;
+        this.listeners = [];
+    }
+
+   subscribe(listenerFunction) {
+    this.listeners.push(listenerFunction);
+
+    return () => {
+        this.listeners = this.listeners.filter(
+            listener => listener !== listenerFunction
+        );
+    };
+}
+
+    setState(newState) {
+        this.state = { ...this.state, ...newState };
+
+        this.listeners.forEach(listener => listener(this.state));
+    }
+}
+
+export const globalStore = new StateStore({
+    cartCount: 0,
+    userTheme: 'light'
+});
